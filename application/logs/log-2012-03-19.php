6bf24432d0bebad4b1bc453931f575a044707532
Rafael V. de Oliveira <elrafael123@gmail.com>
2012-03-19 11:36:36 +0000

    create repository

---
+++ application/logs/log-2012-03-19.php
@@ -0,0 +1,3 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-03-19 11:07:40 --> Severity: Notice  --> Undefined variable: content /Applications/MAMP/htdocs/codeigniter/application/views/templates/default.php 12
