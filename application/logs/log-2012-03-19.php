378f349e9d54b50e7ad58060d19abc48f28f6cad
Rafael V. de Oliveira <elrafael123@gmail.com>
2012-03-19 12:46:51 +0000

    remove unsed log

--- application/logs/log-2012-03-19.php
+++
@@ -1,3 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2012-03-19 11:07:40 --> Severity: Notice  --> Undefined variable: content /Applications/MAMP/htdocs/codeigniter/application/views/templates/default.php 12
