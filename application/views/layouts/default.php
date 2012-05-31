984b61e3e17d4604bd3b4b14a3666e2bc6ce3caf
Rafael V. de Oliveira <elrafael123@gmail.com>
2012-05-31 15:44:03 +0100

    changing the default templates

---
+++ application/views/layouts/default.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<meta name="description" content="">
+	<meta name="keywords" content="" />
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<?php echo $template['body']; ?>
+</body>
+</html>
