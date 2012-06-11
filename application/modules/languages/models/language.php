f0ccece3d38964ab5de06fad00b61a1eeb1b8cea
Rafael V. de Oliveira <elrafael123@gmail.com>
2012-06-11 12:56:40 +0100

    Language model and the test relationship (blog model will not be present)

---
+++ application/modules/languages/models/language.php
@@ -0,0 +1,12 @@
+<?php
+
+class Language extends DataMapper {
+
+	public $has_many = array('blog');
+
+	public function __construct($id = NULL)
+	{
+		parent::__construct($id);
+		Datamapper::add_model_path( array( APPPATH . 'modules/blogs' ) );
+	}
+}
